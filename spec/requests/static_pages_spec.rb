0e74d0822770968ccdadd730f27fd13d3b022762
Arpan Bhattacharya <arpanbee@gmail.com>
2014-09-09 22:26:23 -0400

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,13 @@ describe "Static Pages" do
       expect(page).to have_content('Sample App')
       #response.status.should be(200)
     end
+
+    it "should have the right title" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      #response.status.should be(200)
+    end
   end
 
   describe "Help Page" do
@@ -17,13 +24,27 @@ describe "Static Pages" do
       expect(page).to have_content('Help')
       #response.status.should be(200)
     end
+
+    it "should have the right title" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      #response.status.should be(200)
+    end
   end
 
-    describe "About Page" do
+  describe "About Page" do
     it "should have the content 'About Us'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+      #response.status.should be(200)
+    end
+
+    it "should have the right title" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
       #response.status.should be(200)
     end
   end
