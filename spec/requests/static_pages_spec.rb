2ef9ff1f5c4471a0951c80434549e01113b32432
Arpan Bhattacharya <arpanbee@gmail.com>
2014-09-09 22:04:18 -0400

    add about view

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+  describe "Home Page" do
+    it "should have the content 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+      #response.status.should be(200)
+    end
+  end
+
+  describe "Help Page" do
+    it "should have the content 'Help'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+      #response.status.should be(200)
+    end
+  end
+
+    describe "About Page" do
+    it "should have the content 'About Us'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+      #response.status.should be(200)
+    end
+  end
+
+end
